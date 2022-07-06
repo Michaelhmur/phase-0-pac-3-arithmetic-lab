01c98e3614373c5f2dce0fe8d82e3a408e7cfed3
Michael Murphy <michaelhmurphy99@gmail.com>
2022-07-06 15:14:29 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(x, y) {
+    return x + y;
+  }
+  function subtract(x, y) {
+    return x - y;
+  }
+  function multiply(x, y) {
+    return x * y;
+  }
+  function divide(x,y) {
+    return x / y;
+  }
+  function increment(n) {
+    return n + 1;
+  }
+  function decrement(n) {
+    return n - 1;
+  }
+  function makeInt(n) {
+    return n;
+  }
+function preserveDecimal(n) {
+    return n;}
